8a6d6701ec8912dbc5186e45f3b652bef845b056
Beck Kramer <beck.kramer@trunkclub.com>
2019-08-08 13:40:36 -0500

    Updates server report fetch ahead of consolidated json

--- server/api/reports.js
+++ server/api/reports.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const reports = {
-  R02282019: require('../reports/report-02282019.json'),
-  R03072019: require('../reports/report-03072019--unique.json'),
-  R031119: require('../reports/report-031119--unique.json'),
+const auditFiles = {
+  main: require('../../audits/customer-app/mock.json')
 }
 
 /* GET report listing. */
-router.get('/:id', function(request, response, next) {
-  response.status(200).json(reports[`R${request.params.id}`]);
+router.get('/', function(request, response, next) {
+  response.status(200).json(auditFiles.main);
 });
 
 module.exports = router;
