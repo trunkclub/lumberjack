4371a2e228e78a7a4d8feacc8337109cce100114
Beck Kramer <beck.kramer@trunkclub.com>
2019-09-26 12:16:49 -0500

    Adds violation tally utility and script to package.json

--- utils/ViolationUtils.js
+++ utils/ViolationUtils.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const APP_CONFIG = require('../config/app.json')
 const AUDIT_FOLDER = `./audits/${APP_CONFIG.id}`
 
-var violationGenerator = function* () {
+const violationGenerator = function* () {
   // look in audit folder
   const filenames = fs.readdirSync(`${AUDIT_FOLDER}/route-reports`)
 
@@ -12,7 +12,7 @@ var violationGenerator = function* () {
     const file = fs.readFileSync(`${AUDIT_FOLDER}/route-reports/${filenames[i]}`)
     var json = JSON.parse(file)
 
-    // combine into one json file named with test id
+    // combine into one set of violations
     for(const violation of json){
       yield violation
     }
@@ -26,8 +26,7 @@ module.exports.addRouteToViolation = (violation, route) => {
   return violation
 }
 
-
-module.exports.getUniqueViolations = () => {
+const getUniqueViolations = () => {
   // create unique violation array
   const uniqueViolations = {}
   // loop through each violation entry
@@ -35,32 +34,43 @@ module.exports.getUniqueViolations = () => {
     for(const violation of violations.violations){
       //Does this violation already exist in the uniqueViolations? No, add it
       if (!uniqueViolations[violation.id]) {
-        const routeAdded = this.addRouteToViolation(violation, violations.route)
         uniqueViolations[violation.id] = violation
-      } else {
-        //Yes, lets see if a matching target is in the unique violations
-        const uniqueTargets = uniqueViolations[violation.id].nodes.map(node => {
-          return node.target[0]
-        })
-
-        violation.nodes.forEach(node => {
-          const currentTarget = node.target[0]
-          const alreadyPresent = uniqueTargets.includes(currentTarget)
-
-          //If node target is not in unique violations add route to node and add to uniques
-          if (!alreadyPresent) {
-            node.routes = [violations.route]
-            uniqueViolations[violation.id].nodes.push(node)
-          }else{
-            //if it is in unique violations add route to the unique violation
-            function isTarget(x){
-              return x.target[0] === node.target[0]
-            }
-            const nodeX = uniqueViolations[violation.id].nodes.find(isTarget)
-            nodeX.routes.push(violations.route)
-          }
-        })
       }
+    }
+  }
+  return uniqueViolations // OR create / write to a new JSON file right away
+}
+
+
+module.exports.createUniqueViolationReport = () => {
+  // create unique violation array
+  const uniqueViolations = getUniqueViolations()
+  // loop through each violation entry
+  for(const report of violationGenerator()){
+    for(const violation of report.violations){
+      
+      const uniqueTargets = uniqueViolations[violation.id].nodes.map(node => {
+        return node.target[0]
+      })
+
+      violation.nodes.forEach(node => {
+        const currentTarget = node.target[0]
+        const alreadyPresent = uniqueTargets.includes(currentTarget)
+
+        //If node target is not in unique violations add route to node and add to uniques
+        if (!alreadyPresent) {
+          node.routes = [violations.route]
+          uniqueViolations[violation.id].nodes.push(node)
+        }else{
+          //if it is in unique violations add route to the unique violation
+          function isTarget(x){
+            return x.target[0] === node.target[0]
+          }
+          const nodeX = uniqueViolations[violation.id].nodes.find(isTarget)
+          nodeX.routes.push(violations.route)
+        }
+      })
+      
     }
 
   }
@@ -78,6 +88,68 @@ module.exports.getUniqueViolations = () => {
       }
     }
   )
-  // after all routes have been checked, return object
-  // return uniqueViolations // OR create / write to a new JSON file right away
+}
+
+module.exports.createViolationTallyReport = async() => {
+
+  // Order of severity: minor -> moderate -> serious -> critical
+  const violationsInstancesByRouteByImpact = {
+    critical: 0,
+    minor: 0,
+    moderate: 0,
+    serious: 0,
+  }
+
+  const violationInstancesByImpact = {
+    critical: 0,
+    minor: 0,
+    moderate: 0,
+    serious: 0,
+  }
+
+  const violationsById = {}
+
+  for(const report of violationGenerator()){
+    for(const violation of report.violations){
+      const {
+        id,
+        impact,
+        tags,
+        description,
+        help,
+        helpUrl,
+        nodes
+      } = violation
+
+      if (!Object.keys(violationsById).includes(id)) {
+        violationsById[id] = nodes.length
+      } else {
+        violationsById[id] += nodes.length
+      }
+
+      violationsInstancesByRouteByImpact[impact]++
+
+      nodes.forEach(node => {
+        const {
+          any,
+          all,
+          none,
+          impact,
+          html,
+          target,
+          failureSummary
+        } = node
+
+        violationInstancesByImpact[impact]++
+      })
+    }
+  }
+
+  const violationTally = {
+    date: new Date(),
+    byId: violationsById,
+    byImpact: violationInstancesByImpact,
+  }
+
+  console.log(violationTally)
 }
