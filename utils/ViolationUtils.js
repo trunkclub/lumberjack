461f79061eb9cfe3abec05ce4933a869bbdfbb6f
Beck Kramer <beck.kramer@trunkclub.com>
2019-10-08 08:59:07 -0500

    Pulls unique violation utilities our into a new file

--- utils/ViolationUtils.js
+++ utils/ViolationUtils.js
@@ -3,19 +3,19 @@ const fs = require('fs')
 const APP_CONFIG = require('../config/app.json')
 const AUDIT_FOLDER = `./audits/${APP_CONFIG.id}`
 
-const violationGenerator = function* () {
+module.exports.violationGenerator = function* (reportId) {
   // look in audit folder
   const filenames = fs.readdirSync(`${AUDIT_FOLDER}/route-reports`)
 
   // fetch all json files
   for(const i in filenames){
     const file = fs.readFileSync(`${AUDIT_FOLDER}/route-reports/${filenames[i]}`)
-    var json = JSON.parse(file)
+    const data = JSON.parse(file)
 
-    // combine into one set of violations
-    for(const violation of json){
-      yield violation
-    }
+    // TODO: Have this check reportId arg
+    const mostRecentData = data[data.length-1]
+
+    yield mostRecentData
   }
 }
 
@@ -26,12 +26,12 @@ module.exports.addRouteToViolation = (violation, route) => {
   return violation
 }
 
-const getUniqueViolations = () => {
+module.exports.getUniqueViolations = (reportId) => {
   // create unique violation array
   const uniqueViolations = {}
   // loop through each violation entry
-  for(const violations of violationGenerator()){
-    for(const violation of violations.violations){
+  for(const data of this.violationGenerator(reportId)){
+    for(const violation of data.violations){
       //Does this violation already exist in the uniqueViolations? No, add it
       if (!uniqueViolations[violation.id]) {
         uniqueViolations[violation.id] = violation
@@ -39,54 +39,6 @@ const getUniqueViolations = () => {
     }
   }
   return uniqueViolations // OR create / write to a new JSON file right away
-}
-
-module.exports.createUniqueViolationReport = () => {
-  // create unique violation array
-  const uniqueViolations = getUniqueViolations()
-  // loop through each violation entry
-  for(const report of violationGenerator()){
-    for(const violation of report.violations){
-      
-      const uniqueTargets = uniqueViolations[violation.id].nodes.map(node => {
-        return node.target[0]
-      })
-
-      violation.nodes.forEach(node => {
-        const currentTarget = node.target[0]
-        const alreadyPresent = uniqueTargets.includes(currentTarget)
-
-        //If node target is not in unique violations add route to node and add to uniques
-        if (!alreadyPresent) {
-          node.routes = [violations.route]
-          uniqueViolations[violation.id].nodes.push(node)
-        }else{
-          //if it is in unique violations add route to the unique violation
-          function isTarget(x){
-            return x.target[0] === node.target[0]
-          }
-          const nodeX = uniqueViolations[violation.id].nodes.find(isTarget)
-          nodeX.routes.push(violations.route)
-        }
-      })
-      
-    }
-
-  }
-  fs.writeFile(
-    `${AUDIT_FOLDER}/uniqueViolations.json`,
-    JSON.stringify([uniqueViolations]),
-    'utf8',
-    (error, result) => {
-      if (error) {
-        console.log(' There was an issue writing the report.')
-
-      } else {
-        console.log(' Report created.')
-
-      }
-    }
-  )
 }
 
 module.exports.getViolationTallyData = () => {
